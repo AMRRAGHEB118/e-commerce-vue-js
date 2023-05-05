ff92cd761626e0edf1a775cb8cd314713086ccd7
Amr Ragheb <amrragheb118@gmail.com>
2023-05-05 12:47:12 +0300

    add sorry message

--- e-commerce/src/components/ProductsList.vue
+++ e-commerce/src/components/ProductsList.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="page_container flex flex-col flex-wrap justify-center items-center my-10">
-        <div v-if="products.length > 0" class="list_of_products flex flex-row flex-wrap justify-center items-center gap-3">
+        <div v-show="!sorry_message" class="list_of_products flex flex-row flex-wrap justify-center items-center gap-3">
             <ProductCard v-for="product in products" :key="product.id" :title="product.title" :image="product.cover_image"
                 :price="product.price.toString()" :description="product.description"
                 class="w-64 h-96 rounded overflow-hidden shadow-lg mx-2 my-2" />
         </div>
-        <div v-else class="text-center w-4/5">
-            <h1 class="sorry_message text-5xl">Sorry, there are currently no products available in this category, we are
-                working on having it available soon</h1>
+        <div v-show="sorry_message" class="text-center w-4/5">
+            <h1 class="sorry_message text-5xl">
+                Sorry, there are currently no products available in this category, we are working on
+                having it available soon
+            </h1>
         </div>
     </div>
 </template>
@@ -22,40 +24,42 @@ export default {
     data() {
         return {
             products: [],
+            sorry_message: false,
         }
     },
-    mounted() {
+    beforeMount() {
         (async () => {
             try {
                 const response = await this.$http.get(`/categories/${this.id}/products`)
                 this.products = response.data.data
+                if (this.products.length < 1) {
+                    this.sorry_message = true
+                }
             } catch (error) {
                 console.error(error)
             }
-        })
-            ()
+        })()
     },
 }
-
 </script>
 <style scoped>
 .page_container {
     min-height: 70vh;
 }
 
-@media(max-width: 1024px) {
+@media (max-width: 1024px) {
     .sorry_message {
         font-size: larger;
     }
 }
 
-@media(max-width: 768px) {
+@media (max-width: 768px) {
     .sorry_message {
         font-size: large;
     }
 }
 
-@media(max-width: 426px) {
+@media (max-width: 426px) {
     .sorry_message {
         font-size: medium;
     }
